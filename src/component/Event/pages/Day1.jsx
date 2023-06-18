6b8a1981032f9073e63a9efb7f2a09c3542ca9d5
aswnss-m <aswnss.m@gmail.com>
2023-06-18 12:44:31 +0530

    added events automation

--- src/component/Event/pages/Day1.jsx
+++ src/component/Event/pages/Day1.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import proshow from '../../../assets/proshow.jpg';
+import other from '../../../assets/event.jpg';
+import { day1 } from '../../../constants/links';
 
 function Day1() {
+  const navigate = useNavigate();
+
+  const handleClick = (link) => {
+    window.open(link, '_blank');
+  };
+
+ 
   return (
-    <div>
-      day 1
+    <div className='eventDay'>
+      <div className="proshowEvent" onClick={() => handleClick(day1[0].link)}>
+        <img src={day1[0].poster} className='proshowPoster' alt='Proshow' />
+      </div>
+      <div className="otherEvents">
+        {day1.slice(1).map((link) => (
+          <div className="otherEvent" key={link.id} onClick={() => handleClick(link.link)}>
+            <img src={link.poster} alt="" className='otherEventPoster' />
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Day1
+export default Day1;
