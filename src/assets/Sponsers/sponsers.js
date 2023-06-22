2c2736cbee7e4a1533e3c160f25fea0a54216573
aswnss-m <aswnss.m@gmail.com>
2023-06-22 20:27:12 +0530

    conflicts fixed

--- src/assets/Sponsers/sponsers.js
+++ src/assets/Sponsers/sponsers.js
@@ -1,20 +1,10 @@
 import ramachandran from './ramachandrans.png';
-<<<<<<< HEAD
 import tripShopping from './TripShopin.png';
 import Explorworld from './Explorworld.jpg'
-=======
-import ssw from './ssw.png';
-import tripShopping from './TripShopin.png';
->>>>>>> a9b187b84f5550b1ed11bba15359a8e605f50746
 
 
 export const sponsers = [
     ramachandran,
-<<<<<<< HEAD
     tripShopping,
     Explorworld,
-=======
-    ssw,
-    tripShopping,
->>>>>>> a9b187b84f5550b1ed11bba15359a8e605f50746
 ]
